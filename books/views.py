d1123d0b9f4e381468ab4ae859d9f4fce5e9d350
DudulClaudiul <dudulhimslef@gmail.com>
2026-07-14 20:51:23 +0300

    added Delete part of Book CRUD

--- books/views.py
+++ books/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse, HttpRequest
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 
 from .forms import BookForm
 from .models import Book
@@ -36,4 +36,17 @@ def create_book(request: HttpRequest):
         form = BookForm()
         list1 = [10, 20, 30, 40, 50]
         return render(request, template_name="books/book_form.html", context={"form": form, "list1": list1})
+
+def delete_book(request: HttpRequest, pk: int):
+    book = get_object_or_404(Book, pk=pk)
+
+    if request.method == "POST":
+        book.delete()
+        return redirect("home")
+    else:
+        return render(request, template_name="books/book_confirm_delete.html", context={"book": book})
+
+
+
+
 
