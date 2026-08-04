e0a4ddc5e115864e373593af4699b27a8f8a6d13
DudulClaudiul <dudulhimslef@gmail.com>
2026-08-04 20:55:03 +0300

    added book image and deletion

--- books/apps.py
+++ books/apps.py
@@ -3,3 +3,8 @@ from django.apps import AppConfig
 
 class BooksConfig(AppConfig):
     name = 'books'
+
+    def ready(self):
+        # register signal after django setup is done.
+        import books.signals
+
--- books/forms.py
+++ books/forms.py
@@ -4,7 +4,8 @@ from .models import Book
 class BookForm(forms.ModelForm):
     class Meta:
         model = Book
-        fields = ["title", "author"]
+        fields = ["title", "author", "cover_image"
+                                     ""]
 
 
 
