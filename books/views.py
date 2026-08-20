48ddfb4ffe90844b3864359171b4344c062954c6
DudulClaudiul <dudulhimslef@gmail.com>
2026-08-20 20:57:28 +0300

    added messages and some more tests

--- books/views.py
+++ books/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 from .forms import BookForm
 from .models import Book
@@ -58,6 +59,9 @@ def create_book(request: HttpRequest):
             book.user = request.user
             book.save()
 
+            #notify user that the book has been created
+            messages.success(request, f"Book {book.title} was created")
+
             return redirect("create_book")
 
     else:
@@ -91,6 +95,7 @@ def delete_book(request: HttpRequest, pk: int):
     if request.user.pk == book.user.pk:
         if request.method == "POST":
             book.delete()
+            messages.success(request, f"Book {book.title} was deleted")
             return redirect("home")
         else:
             return render(request, template_name="books/book_confirm_delete.html", context={"book": book})
